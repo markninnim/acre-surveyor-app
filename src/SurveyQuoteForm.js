866e0a89583ebb80c19ff18f6e652c53a637d418
Mark Ninnim <markninnim@Mac-mini.local>
2025-05-01 18:37:36 +0100

    Fix: remove duplicate useState declaration

--- src/SurveyQuoteForm.js
+++ src/SurveyQuoteForm.js
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 
 export default function SurveyQuoteForm() {
-  const [formData, setFormData] = useState({
  const [formData, setFormData] = useState({
   first_name: '',
   last_name: '',
